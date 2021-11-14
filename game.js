1ef340d8eaa72bdb436d98013a14552504f82fba
Chad DeGange <chaddegange@gmail.com>
2021-11-13 20:57:46 -0700

    Add function to determine outcome of alt game

--- game.js
+++ game.js
@@ -9,12 +9,12 @@ class Game {
     this.gameType ;
     this.choices ;
   }
-  computerSelectionClassic() {
+  computerSelection() {
     console.log(this.gameType)
     if(this.gameType === "Classic") {
     this.choices = ["rock", "paper", "scissors"];
   } else if(this.gameType === "Spicy") {
-    this.choices = ["rock", "paper", "scissors", "plane", "car"]
+    this.choices = ["rock", "scissors", "plane", "train", "car"]
   }
   console.log(this.gameType);
   console.log(this.choices);
@@ -45,15 +45,88 @@ class Game {
       this.cpu.wins += 1;
     }
      // this.startNewGame()
+   }
+   determineOutcomeAlt() {
+     if(this.computerSelection === "rock" && this.userSelection === "altRock") {
+     this.result = "It's a draw!";
+   } else if(this.computerSelection === "rock" && this.userSelection === "altScissors") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "rock" && this.userSelection === "altTrain") {
+     this.result = "You win!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "rock" && this.userSelection === "altCar") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "rock" && this.userSelection === "altPlane") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "scissors" && this.userSelection === "altScissors") {
+     this.result = "It's a draw!";
+   } else if(this.computerSelection === "scissors" && this.userSelection === "altTrain") {
+     this.result = "You win!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "scissors" && this.userSelection === "altCar") {
+     this.result = "You win!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "scissors" && this.userSelection === "altRock") {
+     this.result = "You won!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "scissors" && this.userSelection === "altPlane") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "plane" && this.userSelection === "altPlane") {
+     this.result = "It's a draw!";
+   } else if(this.computerSelection === "plane" && this.userSelection === "altTrain") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "plane" && this.userSelection === "altCar") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "plane" && this.userSelection === "altRock") {
+     this.result = "You won!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "plane" && this.userSelection === "altScissors") {
+     this.result = "You won!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "car" && this.userSelection === "altCar") {
+     this.result = "It's a draw!";
+   } else if(this.computerSelection === "car" && this.userSelection === "altScissors") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "car" && this.userSelection === "altRock") {
+     this.result = "You win!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "car" && this.userSelection === "altTrain") {
+     this.result = "You won!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "car" && this.userSelection === "altPlane") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "train" && this.userSelection === "altTrain") {
+     this.result = "It's a draw!";
+   } else if(this.computerSelection === "train" && this.userSelection === "altCar") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "train" && this.userSelection === "altPlane") {
+     this.result = "You won!";
+     this.player1.wins += 1;
+   } else if(this.computerSelection === "train" && this.userSelection === "altRock") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
+   } else if(this.computerSelection === "train" && this.userSelection === "altScissors") {
+     this.result = "You lost!";
+     this.cpu.wins += 1;
 // }
-console.log(this.userSelection);
-console.log(this.computerSelection);
-console.log(this.result);
 //   // startNewGame(){
 //   //
 //   //
 //   }
-}
+console.log(this.userSelection);
+console.log(this.computerSelection);
+console.log(this.result);
+    }
+  }
 }
 
 
--- main.js
+++ main.js
@@ -8,11 +8,13 @@ var chooseGameTitle = document.querySelector("#chooseGameTitle");
 var classicImages = document.querySelector("#classicGameBoard");
 var altImages = document.querySelector("#altGameBoard");
 var classicFighterSelection = document.querySelector("#classicGameBoard");
+var altFighterSelection = document.querySelector("#altGameBoard");
 var paperSelectionClassic = document.querySelector("#paper");
 var scissorsSelectionClassic = document.querySelector("#scissors");
 var rockSelectionClassic = document.querySelector("#rock");
 var loadPlayerData = document.querySelector("#humanWinsSection");
 var loadCpuData = document.querySelector("#cpuWinsSection");
+
 // Event Handlers
 window.addEventListener("load", function() {
   onPageLoad()
@@ -20,14 +22,17 @@ window.addEventListener("load", function() {
 classicGameSelection.addEventListener("click", switchClassicFighterView);
 altGameSelection.addEventListener("click", switchAltFighterView);
 classicFighterSelection.addEventListener("click", function(event){
-  identifyUserSelection(event)
+  fighterSelectionAssignment(event)
+});
+altFighterSelection.addEventListener("click", function(event){
+  fighterSelectionAssignment(event)
 });
 
 function onPageLoad() {
   currentGame = new Game();
   currentGame.player1.retrieveWinsFromStorage();
   currentGame.cpu.retrieveWinsFromStorage();
-}
+};
 
 function switchClassicFighterView() {
   currentGame.gameType = "Classic";
@@ -35,8 +40,7 @@ function switchClassicFighterView() {
   classicImages.classList.remove("hidden");
   chooseGameTitle.innerText = "PICK YOUR FIGHTER!";
   loadPlayer();
-}
-
+};
 
 function switchAltFighterView() {
   currentGame.gameType = "Spicy";
@@ -44,29 +48,39 @@ function switchAltFighterView() {
   altImages.classList.remove("hidden");
   chooseGameTitle.innerText = "PICK YOUR FIGHTER!";
   loadCpu();
-}
-function identifyUserSelection(event) {
+};
+
+function fighterSelectionAssignment(event) {
   currentGame.userSelection = event.target.id;
-  currentGame.computerSelectionClassic();
+  currentGame.computerSelection();
+  getOutcome();
+};
+
+function getOutcome() {
+  if (currentGame.gameType === "Classic") {
   currentGame.determineOutcomeClassic();
-  displayOutcome()
+} else if (currentGame.gameType === "Spicy") {
+  currentGame.determineOutcomeAlt();
 }
+  displayOutcome();
+};
 
 function loadPlayer() {
   loadPlayerData.classList.add("hidden");
   loadPlayerData.innerHTML += `
     <h2 id="humanToken">${currentGame.player1.token}</h2>
     <h2 id="humanWinsColumn">${currentGame.player1.name}</h2>
-    <h3 id="humanWinsCounter">${currentGame.player1.wins}</h3>
-    `
-}
+    <h3 id="humanWinsCounter">${currentGame.player1.wins}</h3>    `
+};
+
 function loadCpu() {
   loadCpuData.classList.add("hidden");
   loadPlayerData.innerHTML += `
     <h2 id="computerToken">${currentGame.cpu.token}</h2>
     <h2 id="computerWinColumn">${currentGame.cpu.name}</h2>
     <h3 id="computerWinCounter">${currentGame.cpu.wins}</h3>`
-}
+};
+
 function displayOutcome() {
 
 }
